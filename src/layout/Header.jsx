c3fa9c410372a9dff735ffeec88a15e79ce69143
Alisa <alisa.luoma@hotmail.fi>
2023-02-28 09:55:27 +0200

    New Project start

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../images/logo192.png'
+
+const Header = () => {
+  return(
+    <div className="col-12 py-3 pl-2" style={{borderBottom: "1px solid #7777"}}>
+      <img src={logo} style={{height: "35px", verticalAlign: "top"}} alt="React logo"></img>
+      <span className="h2 pt-4 m-2 text-white-50">Contactopedia</span>
+    </div>
+  )
+}
+
+export default Header;
