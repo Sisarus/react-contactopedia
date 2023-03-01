4471aa02526bd78ce640d60a15c96b6a98dd644d
Alisa <alisa.luoma@hotmail.fi>
2023-03-01 08:41:19 +0200

    Contactslist

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -8,6 +8,35 @@ import GeneralContacts from './GeneralContacts';
 import Footer from '../Layout/Footer';
 
 class ContactIndex extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      contacList:[
+        {
+          id: 1,
+          name: "Leslie Knope",
+          phone: "1234567",
+          email: "Leslie.Knope@pawnee.com",
+          isFavorite: false,
+        },
+        {
+          id: 2,
+          name: "Ron Swanson",
+          phone: "1234567",
+          email: "Ron.Swanson@pawnee.com",
+          isFavorite: true,
+        },
+        {
+          id: 3,
+          name: "April Ludgate",
+          phone: "1234567",
+          email: "April.Ludgate@pawnee.com",
+          isFavorite: false,
+        }
+      ]
+    }
+  }
+
   render(){
     return(
       <div>
