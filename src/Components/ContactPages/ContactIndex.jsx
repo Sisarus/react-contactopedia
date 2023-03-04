3c38d50e71910b1022c2291bd587d7b1032e9e38
Alisa <alisa.luoma@hotmail.fi>
2023-03-04 10:29:05 +0200

    Show error msg

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -44,7 +44,7 @@ class ContactIndex extends React.Component{
       return {status: 'failure', msg:"Pleace Enter a Phone number"};
     }
     const duolicateRecord = this.state.contactList.filter((x)=>{
-      if(x.name==newContact.name || x.phone==newContact.phone){
+      if(x.name==newContact.name && x.phone==newContact.phone){
         return true;
       }
     });
